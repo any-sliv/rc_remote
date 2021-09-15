0f7fc7665e74d13857716e407dce14cb7703b533
Maciej Sliwinski <mjsliwinski@outlook.com>
2021-09-15 21:01:38 +0200

    All CXX well built

--- App/nrf24/radioTask.hpp
+++ App/nrf24/radioTask.hpp
@@ -1,1 +1,12 @@
-void radioTask(void *argument);
+/*
+ * radioTask.hpp
+ *
+ *  Created on: Aug 11, 2021
+ *      Author: macsli
+ */
+
+#ifndef INC_RADIOTASK_H_
+#define INC_RADIOTASK_H_
+
+
+#endif /* INC_RADIOTASK_H_ */
