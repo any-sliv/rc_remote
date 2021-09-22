5fd51402198431ed991ad3cda8dcd00e9ca3af47
Maciej Sliwinski <mjsliwinski@outlook.com>
2021-09-22 21:21:08 +0200

    radioTask schedules radio operations

--- App/nrf24/radioTask.cpp
+++ App/nrf24/radioTask.cpp
@@ -4,19 +4,33 @@
  *  Created on: Sep 15, 2021
  *      Author: macsli
  */
-
-#include "radioTask.hpp"
-#include "radioClass.hpp"
-
 #ifdef __cplusplus 
 extern "C" { 
 #endif 
 
-void radioTask(void *argument)
-{
-    //asm("nop");
-}
+#include "radioTask.hpp"
+#include "radioClass.hpp"
+#include "cmsis_os.h"
+#include "task.h"
 
+
+#define RADIO_TASK_TIME_INTERVAL 5000
+
+void radioTask(void const * argument)
+{
+    uint32_t taskTimer = (uint32_t) osKernelGetTickCount + RADIO_TASK_TIME_INTERVAL;
+    NRF24 radio;
+    void * rxData;
+
+    for(;;) {
+        if(radio.IsAvailable()) {
+            radio.Read(rxData);
+        }
+        radio.Write((void *)"x");
+
+        vTaskDelay(taskTimer);
+    }
+}
 
 
 // --------------------------------------------------------
